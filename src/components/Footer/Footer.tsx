d8bdd20bf92ea3cc01ac7457431919e535e98c04
loipv03 <phanvanloi1522003@gmail.com>
2023-07-24 21:54:41 +0700

    update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,18 @@
+import { AiOutlineTwitter } from "react-icons/ai";
+import { BsGithub } from "react-icons/bs";
+import { BsFacebook } from "react-icons/bs";
+import { AiOutlineInstagram } from "react-icons/ai";
 import classNames from "classnames/bind";
 
 import styles from "./footer.module.scss";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const Footer = () => {
   return (
     <footer className={cx("footer")}>
-      <div className={cx("footer_item")}>
+      <div className={cx("footer_subscribe")}>
         <div className={cx("subscribe")}>
           <img src="image/subscribe.png" alt="" />
           <form className={cx("form")}>
@@ -18,16 +23,73 @@ const Footer = () => {
           </form>
         </div>
       </div>
-      {/* <div className={cx("footer_item")}>
-        <div className={cx("contact")}>contact us</div>
-        <div className={cx("logo")}>
-          <img src="image/logo.png" alt="" />
+      <div className={cx("footer_about")}>
+        <div className={cx("contact")}>
+          <h6>contact us</h6>
+          <ul>
+            <li>
+              <span>Email: </span>phanvanloi1522003@gmail.com
+            </li>
+
+            <li>
+              <span>Phone: </span>0364336088
+            </li>
+
+            <li>
+              <span>Address: </span>Nam Từ Liêm - Hà Nội
+            </li>
+          </ul>
         </div>
-        <div className={cx("utility")}>Utility Pages</div>
+        <div className={cx("footer_widget")}>
+          <div className={cx("img")}>
+            <img src="image/logo.png" alt="" />
+          </div>
+          <div className={cx("text")}>
+            Simply dummy text of the printing and typesetting industry. Lorem
+            Ipsum simply dummy text of the printing{" "}
+          </div>
+          <div className={cx("icon")}>
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BsFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BsGithub />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineTwitter />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className={cx("footer_link")}>
+          <h6>Utility Pages</h6>
+          <ul>
+            <li>Style Guide</li>
+            <li>404 Not Found</li>
+            <li>Password Protected</li>
+            <li>Licences</li>
+            <li>Changelog</li>
+          </ul>
+        </div>
       </div>
-      <div className={cx("footer_item")}>
-        <div className={cx("about_banner")}></div>
-      </div> */}
+      <div className={cx("footer_copyright")}>
+        <div className={cx("text")}>
+          Copyright © <span>Organick</span> | Designed by{" "}
+          <span>VictorFlow</span> Templates - Powered by <span>Webflow</span>
+        </div>
+      </div>
     </footer>
   );
 };
